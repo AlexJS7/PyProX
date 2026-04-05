3340ce895ae48b15bc0f386b0252f3363e16257a
Oleksandr Yakovchenko <psp4dream@gmail.com>
2026-04-05 18:27:48 +0300

    seconds_to_units

--- ex11.py
+++ ex11.py
@@ -13,28 +13,26 @@ HOUR = 60 * MINUTE
 DAY = HOUR * 24
 
 
+def seconds_to_units(total_seconds, unit_seconds):
+    units = total_seconds // unit_seconds
+    remaining_seconds = total_seconds % unit_seconds
+
+    return units, remaining_seconds
+
+
 def get_hours_minutes_seconds(total_seconds):
     if total_seconds == 0:
         return '0s'
 
-    # how many seconds left
-    s = total_seconds
-
-    d = total_seconds // DAY
-    s = s - d * DAY
-
-    h = s // HOUR
-    s = s - h * HOUR
-
-    m = s // MINUTE
-    s = s - m * MINUTE
+    d, s = seconds_to_units(total_seconds, DAY)
+    h, s = seconds_to_units(s, HOUR)
+    m, s = seconds_to_units(s, MINUTE)
 
     hms = [f'{d}d', f'{h}h', f'{m}m', f'{s}s']
 
     return ' '.join(item for item in hms if item[0] != '0')
 
 
-assert get_hours_minutes_seconds(90) == '1m 30s'
 assert get_hours_minutes_seconds(30) == '30s'
 assert get_hours_minutes_seconds(60) == '1m'
 assert get_hours_minutes_seconds(90) == '1m 30s'
@@ -43,3 +41,4 @@ assert get_hours_minutes_seconds(3601) == '1h 1s'
 assert get_hours_minutes_seconds(3661) == '1h 1m 1s'
 assert get_hours_minutes_seconds(90042) == '1d 1h 42s'
 assert get_hours_minutes_seconds(0) == '0s'
+assert get_hours_minutes_seconds(1) == '1s'
