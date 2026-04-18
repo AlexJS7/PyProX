b3fd9e700b4f7c5b8216fa90bb1234c2c60c257b
Oleksandr Yakovchenko <psp4dream@gmail.com>
2026-04-18 15:53:36 +0300

    ex19: Password Generator

--- ex19_password_generator.py
+++ ex19_password_generator.py
@@ -14,8 +14,6 @@ UPPER_LETTERS = LOWER_LETTERS.upper()
 NUMBERS = ''.join(str(i) for i in range(0, 10))
 SPECIAL = '~!@#$%^&*()_+'
 
-print(LOWER_LETTERS, UPPER_LETTERS, NUMBERS)
-
 
 def generate_password(length: int) -> str:
     """
@@ -23,7 +21,7 @@ def generate_password(length: int) -> str:
     ___________
     Parameters:
         length (int):
-            Number of characters the generated password should have. 
+            Number of characters of generated password. 
     ___________
     Returns:
         out (str):
@@ -34,7 +32,12 @@ def generate_password(length: int) -> str:
                 - one special character: ~!@#$%^&*()_+
     """
 
+    allowed_char_groups = (LOWER_LETTERS, UPPER_LETTERS, NUMBERS, SPECIAL)
+
+    allowed_char_groups_len = len(allowed_char_groups)
+
     min_password_length = 12
+
     final_length = length
 
     if (not isinstance(length, float) and not isinstance(length, int)) or length < min_password_length:
@@ -42,33 +45,35 @@ def generate_password(length: int) -> str:
     elif isinstance(length, float):
         final_length = round(length)
 
-    result = ''
+    result = []
 
-    for i in range(0, final_length + 1):
-        # every fourth takes special
-        if i % 4 == 0:
-            random_idx = random.randint(0, len(SPECIAL))
+    for i in range(0, final_length):
+        group_idx = i % allowed_char_groups_len
+        group_chars_len = len(allowed_char_groups[group_idx])
+        rand_char = allowed_char_groups[group_idx][random.randint(
+            0, group_chars_len - 1)]
+        result.append(rand_char)
 
-        # every third takes number
-            random_idx = random.randint(0, len(NUMBERS))
+    random.shuffle(result)
 
-        # todo
+    return ''.join(result)
 
 
-# assert len(generate_password(8)) == 12
-# pw = generate_password(14)
-# assert len(pw) == 14
-# hasLowercase = False
-# hasUppercase = False
-# hasNumber = False
-# hasSpecial = False
-# for character in pw:
-#  if character in LOWER_LETTERS:
-#     hasLowercase = True
-#  if character in UPPER_LETTERS:
-#     hasUppercase = True
-#  if character in NUMBERS:
-#     hasNumber = True
-#  if character in SPECIAL:
-#     hasSpecial = True
-# assert hasLowercase and hasUppercase and hasNumber and hasSpecial
+assert len(generate_password(8)) == 12
+pw = generate_password(14)
+assert len(pw) == 14
+
+hasLowercase = False
+hasUppercase = False
+hasNumber = False
+hasSpecial = False
+for character in pw:
+    if character in LOWER_LETTERS:
+        hasLowercase = True
+    if character in UPPER_LETTERS:
+        hasUppercase = True
+    if character in NUMBERS:
+        hasNumber = True
+    if character in SPECIAL:
+        hasSpecial = True
+assert hasLowercase and hasUppercase and hasNumber and hasSpecial
